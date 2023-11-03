4a70480b251ddfe8f7cf5475aa47fc5fe74e780e
maot2001 <106125010+maot2001@users.noreply.github.com>
2023-11-03 02:44:36 -0700

    feat: jwt v1

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace AIRCOM.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly DBContext _context;
+        private IConfiguration _config;
+        public LoginController(BDContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+        
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Login(IFormCollection collection)
+        {
+            var user = await _context.Users.
+                SingleOrDefaultAsync(x => x.Email == collection["Email"] && x.Pwd == collection["Pwd"]);
+
+            if (user is null)
+                return BadRequest(new { message = "Credenciales invalidas" });
+
+            string jwtToken = GenerateToken(user);
+
+            return Ok(new { token = jwtToken });
+        }
+
+        private string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Name.ToString()),
+                new Claim(ClaimTypes.Email, user.Email.ToString()),
+                new Claim("UserType", user.Role.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("JWT:Key").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var securityToken = new JwtSecurityToken(
+                                claims: claims,
+                                expires: DateTime.Now.AddMinutes(60),
+                                signingCredentials: creds);
+
+            string token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return token;
+        }
+
+
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LoginController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LoginController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
