dfee4d7d820604b5bb2e842c5448e06ab8b21d17
maot2001 <106125010+maot2001@users.noreply.github.com>
2023-11-25 13:15:13 -0800

    Revisado RepairS

--- Services/RepairService.cs
+++ Services/RepairService.cs
@@ -24,7 +24,7 @@ namespace AIRCOM.Services
         public async Task Create(RepairDTO repair)
         {
             var repairDB = _mapper.Map<Repair>(repair);
-            repairDB.RepairID = 0;
+
             _context.Repairs.Add(repairDB);
             await _context.SaveChangesAsync();
         }
@@ -32,25 +32,30 @@ namespace AIRCOM.Services
         public async Task Edit(RepairDTO repair)
         {
             var repairDB = await GetRepairById(repair.RepairID);
+
             repairDB.Name = repair.Name;
             repairDB.Description = repair.Description;
+
             await _context.SaveChangesAsync();
         }
 
         public async Task Delete(int repairId)
         {
             var repairDB = await GetRepairById(repairId);
+
             _context.Repairs.Remove(repairDB);
             await _context.SaveChangesAsync();
         }
 
         // --------------------------------------------------------------
 
-        private async Task<Repair> GetRepairById(int repairId)
+        private async Task<Repair> GetRepairById(int? repairId)
         {
             var repairDB = await _context.Repairs.FindAsync(repairId);
+
             if (repairDB is null)
-                throw new Exception();
+                throw new Exception("La reparación no existe");
+
             return repairDB;
         }
 
