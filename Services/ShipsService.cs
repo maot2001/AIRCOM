71ac50740174bee054f1fcb09f5ad8091c6a9646
maot2001 <106125010+maot2001@users.noreply.github.com>
2023-11-25 17:20:07 -0800

    Revisado ShipsS

--- Services/ShipsService.cs
+++ Services/ShipsService.cs
@@ -15,24 +15,26 @@ namespace AIRCOM.Services
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<ShipsDTO>> Get(string userId = "")
+        public async Task<IEnumerable<ShipsDTO>> Get(string? userId = null)
         {
             List<Ships> ships;
-            if (userId == "")
+
+            if (userId is null)
                 ships = await _context.Shipss.ToListAsync();
+
             else
                 ships = await _context.Shipss.Where(s => s.ClientID == int.Parse(userId)).ToListAsync();
+
             return _mapper.Map<List<ShipsDTO>>(ships);
         }
 
         public async Task Create(ShipsDTO ship)
         {
             var ships = _mapper.Map<Ships>(ship);
+
             if (ship.ClientID != 0)
-            {
-                var client = await Errors(ship.ClientID);
-                ships.Clients = client;
-            }
+                await Errors(ship.ClientID);
+
             _context.Shipss.Add(ships);
             await _context.SaveChangesAsync();
         }
@@ -40,22 +42,23 @@ namespace AIRCOM.Services
         public async Task Edit(ShipsDTO ship)
         {
             var shipBD = await GetShipById(ship.Plate);
+
             if (ship.ClientID != 0)
-            {
-                var client = await Errors(ship.ClientID);
-                shipBD.Clients = client;
-            }
+                await Errors(ship.ClientID);
+
             shipBD.Model = ship.Model;
             shipBD.Capacity = ship.Capacity;
             shipBD.Crew = ship.Crew;
             shipBD.ClientID = ship.ClientID;
             shipBD.State = ship.State;
+
             await _context.SaveChangesAsync();
         }
 
         public async Task Delete(string id)
         {
             var ship = await GetShipById(id);
+
             _context.Shipss.Remove(ship);
             await _context.SaveChangesAsync();
         }
@@ -64,17 +67,19 @@ namespace AIRCOM.Services
         private async Task<Ships> GetShipById(string id)
         {
             var ship = await _context.Shipss.FindAsync(id);
+
             if (ship is null)
-                throw new Exception();
+                throw new Exception("El avión no existe");
+
             return ship;
         }
 
-        private async Task<Client> Errors(int id)
+        private async Task Errors(int? id)
         {
             var client = await _context.Clients.FindAsync(id);
+
             if (client is null)
-                throw new Exception();
-            return client;
+                throw new Exception("El cliente no existe");
         }
     }
 }
