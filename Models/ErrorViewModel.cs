6daf1e5ca39019ca6ba3b74da2803aef0c357d02
maot2001 <106125010+maot2001@users.noreply.github.com>
2023-11-30 13:16:03 -0800

    Delete Models/ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AIRCOM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
