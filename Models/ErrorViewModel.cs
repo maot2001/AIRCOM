21efe584225e07dd98eca5dd35ad26f93e0794c9
maot2001 <106125010+maot2001@users.noreply.github.com>
2023-11-30 19:24:21 -0800

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AIRCOM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
