3a9175172d473b3ac715c4d754dacd29b5b45a4b
javierrodriguezsanchez <105332166+javierrodriguezsanchez@users.noreply.github.com>
2023-12-01 22:58:36 -0500

    Autodetectando rol en login, Admin ordenado

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -21,12 +21,11 @@ namespace AIRCOM.Controllers
             return View("Admin");
         }
         
-        public IActionResult Init(int page = 1, int lugar_del_error = 0, string error = "")
+        public IActionResult Init(int lugar_del_error = 0, string error = "")
         {
-            ViewData["page"] = page;
             ViewData["lugar_del_error"] = lugar_del_error;
             ViewData["error"] = error;
-            return View("Admin");
+            return View("Administrar");
         }
 
         public IActionResult Comprobe(string pwd)
@@ -35,12 +34,14 @@ namespace AIRCOM.Controllers
             {
                 return RedirectToAction(nameof(Init));
             }
-            ViewData["page"] = 0;
             ViewData["lugar_del_error"] = 1;
             ViewData["error"] = "Contraseña inválida";
             return View("Admin");
         }
-
+        public IActionResult ChangePage(string page)
+        {
+            return View("Estadisticas");
+        }
         private async Task<string> ObtenerContenidoDeLaSolicitud(HttpRequest request)
         {
             using (var reader = new StreamReader(request.Body))
