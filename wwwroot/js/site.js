005d345aba1c38d75ef20a2f5681cd6ac1c855a1
javierrodriguezsanchez <105332166+javierrodriguezsanchez@users.noreply.github.com>
2023-12-04 00:39:39 -0500

    Haciendo direccion dinamico

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function hide() {
+    document.getElementById('add').style.display = 'none';
+}
+
+function show() {
+    document.getElementById('add').style.display = 'block';
+}
+
+function swap(id, ElBoleano) {
+    if (ElBoleano) {
+        document.getElementById(id).style.display = 'none';
+        document.getElementById("a" + id).style.display = 'block';
+    }
+    else {
+        document.getElementById(id).style.display = 'block';
+        document.getElementById("a" + id).style.display = 'none';
+    }
+}
